d843dfa88abf3c67422d20a15a5981d07997c6c6
Jung Chu <jungchu76@gmail.com>
2024-06-21 20:55:24 +0800

    feat: about 頁面圖片輪播功能

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,23 +1,40 @@
 <template>
-    <div id="about">
-        <Toolbar></Toolbar>
+    <div>
+        <div id="about">
+            <Toolbar></Toolbar>
 
-        <div id="description">
-            <img class="description_img" src="/src/assets/about_description.jpg" alt="about description">
-            <div class="description_text">
-                覓巷是一家藏身於城市一隅的特色餐廳，為您提供獨特的用餐體驗。<br/>
-                我們精心挑選新鮮食材，融合傳統與創新的烹飪手法，每一道菜肴都凝聚了廚師的心血與靈感。<br/>
-                來到覓巷，您不僅能享受美食，更能在這裡找到一片遠離喧囂的心靈淨土。<br/>
-                無論是家庭聚餐、朋友聚會，還是浪漫約會，<br/>
-                我們都致力於為您創造難忘的時光。
+            <div id="description">
+                <img class="description_img" src="/src/assets/about_description.jpg" alt="about description">
+                <div class="description_text">
+                    覓巷是一家藏身於城市一隅的特色餐廳，為您提供獨特的用餐體驗。<br/>
+                    我們精心挑選新鮮食材，融合傳統與創新的烹飪手法，每一道菜肴都凝聚了廚師的心血與靈感。<br/>
+                    來到覓巷，您不僅能享受美食，更能在這裡找到一片遠離喧囂的心靈淨土。<br/>
+                    無論是家庭聚餐、朋友聚會，還是浪漫約會，<br/>
+                    我們都致力於為您創造難忘的時光。
+                </div>
+            </div>
+
+            <div id="pictures">
+                <div id="left_col" class="pic_col">
+                    <img 
+                        v-for="n in 5"
+                        :key="'about_l_' + n"
+                        class="img_item" 
+                        :src="`/src/assets/about_l_${n}.jpg`" 
+                        alt="picture"
+                    >
+                </div>
+                <div id="right_col" class="pic_col">
+                    <img 
+                        v-for="n in 4"
+                        :key="'about_r_' + n"
+                        class="img_item" 
+                        :src="`/src/assets/about_r_${n}.jpg`" 
+                        alt="picture"
+                    >
+                </div>
             </div>
         </div>
-
-        <div id="pictures">
-            <div class="left_col">1</div>
-            <div class="right_col">2</div>
-        </div>
-
         <Footer></Footer>
     </div>
 </template>
@@ -31,7 +48,8 @@
 #about {
     position: relative;
     width: 100%;
-    min-height: 100vh;
+    min-height: calc(100vh - 20px); //因為footer高度大約為 20px
+    height: 100%;
     background-color: rgb(36, 36, 36);
 
     #description {
@@ -40,7 +58,7 @@
         justify-content: center;
         align-items: center;
         width: 100%;
-        height: 700px;
+        height: 600px;
         
         .description_img {
             display: block; // 為了移除多餘空白
@@ -66,13 +84,28 @@
     #pictures {
         display: flex;
         justify-content: center;
-        align-items: center;
-        margin: 30px 0 0 0;
-    }
+        height: 1200px;
+        margin: 30px 0;
+        overflow: hidden;
 
-    Footer {
-        position: absolute;
-        bottom: 0;
+        .pic_col {
+            width: 600px;
+            margin: 0 20px;
+            // background-color: aquamarine;
+
+            .img_item {
+                display: block;
+                width: 100%;
+                margin-bottom: 20px;
+            }
+        }
+
+        #left_col {
+            animation: slideLeftCol 25s linear infinite;
+        }
+        #right_col {
+            animation: slideRightCol 25s linear infinite;
+        }
     }
 }
 
@@ -83,6 +116,24 @@
     }
     to {
         opacity: 1;
+    }
+}
+
+@keyframes slideLeftCol {
+    from {
+        margin-top: 0;
+    }
+    to {
+        margin-top: -1730px;
+    }
+}
+
+@keyframes slideRightCol {
+    from {
+        margin-top: -1400px;
+    }
+    to {
+        margin-top: 0;
     }
 }
 </style>
