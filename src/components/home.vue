408e16d515b2ce7929ef0b02efd4a0a1d6157155
Jung Chu <jungchu76@gmail.com>
2024-06-21 20:54:42 +0800

    feat: 新增preload image

--- src/components/home.vue
+++ src/components/home.vue
@@ -17,7 +17,15 @@
         preLoadImg();
     });
     const preLoadImg = () => {
-        const imgNameList = ['menu_background.jpg', 'menu.jpg', 'about_description.jpg'];
+        const imgNameList = [
+            'menu_background.jpg', 
+            'menu.jpg', 
+            'about_description.jpg',
+            'about_l_1.jpg', // 依照最先被看見的順序
+            'about_r_4.jpg',
+            'about_l_2.jpg',
+            'about_r_3.jpg',
+        ];
         setTimeout(() => {
             imgNameList.forEach(imgName => {
                 const image = new Image();
@@ -28,6 +36,7 @@
 </script>
 <style lang='scss' scoped>
 #home {
+    position: relative;
     width: 100%;
     min-height: 100vh;
     background-color: rgb(36, 36, 36);
